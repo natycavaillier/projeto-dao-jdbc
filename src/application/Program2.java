561a8f5ec423646fe12359b22beb8f180afe7c5a
Natalia Cavaillier <nataliaabreu2000@hotmail.com>
2022-09-05 23:44:43 -0300

    Implementação do findAll para o department

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -14,6 +16,9 @@ public class Program2 {
 		Department department = departmentDao.findById(1);
 		System.out.println(department);
 		
+		System.out.println("\n==== TEST 2: department findAll ====");
+		List<Department> list = departmentDao.findAll();
+		list.forEach(System.out::println);
 		
 	}
 
